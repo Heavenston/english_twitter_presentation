955fd9e33d2515ac9987946b4ac7ead2ed1dbbfc
Heavenston <malolegendrelemaire@@gmail.com>
2022-01-31 20:15:33 +0100

    Was a bad idea

--- src/tweets.ts
+++ src/tweets.ts
@@ -19,37 +19,8 @@ Not sure anyone has heard but,
 
 I resigned from Twitter
 `,
-
-    mainComment: 1,
     commentList: [],
 };
-
-startTweet.commentList?.push({
-    user: troll,
-    timestamp: Date.now(),
-    likes: 312200,
-    retweets: 66900,
-    quoteTweets: 37900,
-    showFullThread: true,
-
-    content: `
-Nobody cares
-
-+ ratio
-`,
-
-    commentList: [
-        {
-            user: Users.fake(),
-            timestamp: Date.now(),
-            likes: 312200,
-            retweets: 66900,
-            quoteTweets: 37900,
-
-            content: "Please stop",
-        },
-    ],
-});
 
 startTweet.commentList?.push({
     user: Users.twitter,
@@ -82,6 +53,32 @@ startTweet.commentList?.push({
                     content: "Not",
                 },
             ],
+        },
+    ],
+});
+
+startTweet.commentList?.push({
+    user: troll,
+    timestamp: Date.now(),
+    likes: 312200,
+    retweets: 66900,
+    quoteTweets: 37900,
+
+    content: `
+Nobody cares
+
++ ratio
+`,
+
+    commentList: [
+        {
+            user: Users.fake(),
+            timestamp: Date.now(),
+            likes: 312200,
+            retweets: 66900,
+            quoteTweets: 37900,
+
+            content: "Please stop",
         },
     ],
 });
